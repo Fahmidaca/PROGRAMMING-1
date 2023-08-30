ec984a0dc187bd9a972bc7a94e3896958bd894f9
Fahmida Rahman <141256088+Fahmidaca@users.noreply.github.com>
2023-08-30 11:21:48 +0600

    text file added by me

---
+++ addTwoNumbers.1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+int a,b,s;
+printf("ENTER THE FIRST NUMBER:");
+scanf("%d",&a);
+printf("ENTER THE SECOND NUMBER:");
+scanf("%d",&b);
+s=a+b;
+printf("THE SUMMATION OF TWO NUMBERS IS:%d",s);
+scanf("%d",&s);
+return 0;
+
+}
