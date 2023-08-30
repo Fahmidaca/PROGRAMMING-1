f6173729768370d5bcfdf87913eda2deb68bffb0
Fahmida Rahman <141256088+Fahmidaca@users.noreply.github.com>
2023-08-30 11:27:19 +0600

    text file added by me

---
+++ multiplication.2.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+float a,b,m;
+printf("ENTER THE FIRST NUMBER:");
+scanf("%f",&a);
+printf("ENTER THE SECOND NUMBER:");
+scanf("%f",&b);
+m=a*b;
+printf("THE MULTIPLICATION OF TWO NUMBERS IS:%f",m);
+scanf("%d",&m);
+return 0;
+
+}
