80963184e73faa2e5a1561f3513af67247863578
Fahmida Rahman <141256088+Fahmidaca@users.noreply.github.com>
2023-08-30 11:36:59 +0600

    text file added by me

---
+++ size.1.c
@@ -0,0 +1,17 @@
+//C PROGRAMM TO FIND THE SIZE OF int,float,double AND char
+#include<stdio.h>
+#include<float.h>
+int main()
+{
+    printf("SIZE OF THE int DATA TYPE :%d\n",sizeof(int));
+    printf("SIZE OF THE float DATA TYPE :%d\n",sizeof(float));
+    printf("SIZE OF THE double DATA TYPE :%d\n",sizeof(double));
+    printf("SIZE OF THE long DATA TYPE :%d\n",sizeof(long));
+    printf("SIZE OF THE unsigned long DATA TYPE :%d\n",sizeof(unsigned long));
+    printf("SIZE OF THE long double DATA TYPE :%d\n",sizeof(long double));
+    printf("SIZE OF THE short int DATA TYPE :%d\n",sizeof(short int));
+    printf("SIZE OF THE char DATA TYPE :%d\n",sizeof(char));
+
+
+    return 0;
+}
